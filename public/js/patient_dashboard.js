92164f239721f61a385d7a7b7cb556876c9b964a
V-T-Soorea <vishnusoorea@gmail.com>
2020-07-12 14:25:23 +0530

    Patient dashboard changes

--- public/js/patient_dashboard.js
+++ public/js/patient_dashboard.js
@@ -1,4 +1,14 @@
-document.getElementById("reading").addEventListener("click",function(){
-    alert()
-});
+var bp = document.getElementById("bp");
+var temp = document.getElementById("temp");
+var sugar = document.getElementById("sugar");
+var new_reading = document.getElementById("reading");
 
+new_reading.addEventListener("click",function(){
+    
+    var firebaseDef = firebase.database().ref();
+    firebaseDef.child("Text").set("hello")
+    // var bpValue = bp.value
+    // firebaseDef.push.set(bpValue);
+    // firebaseDef.push.set(temp.value);
+    // firebaseDef.push.set(sugar.value);
+});
